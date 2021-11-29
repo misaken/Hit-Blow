d0fc8163ace1328aa85a6bc7d0578e3ef4a5bdfa
Kento <ghy4hz@gamil.com>
2021-11-29 21:25:36 +0900

    テキストをjsonに変更

--- main.py
+++ main.py
@@ -1,136 +1,87 @@
-import random
+import random, json
 
+print('*/*/*/*/*/数字当てゲーム/*/*/*/*/*\n'\
+      '--<ルール説明>---------------------------------------------------------\n'\
+      '|  0 ~ 9からなる任意の桁数の値を当てよう！                            |\n'\
+      '|  入力した数のうち、値も順番も完全に一致してるものの個数を「Hit」、  |\n'\
+      '|  値はあっているが順番が間違えているものの個数を「Blow」とする。     |\n'\
+      '|                                                                     |\n'\
+      '|  制限回数以内に値を当てることができればゲームクリア！！             |\n'\
+      '|  最速記録の場合はレコードに記録されます。                           |\n'\
+      '-----------------------------------------------------------------------')
 while True:
     options=input('Hit&Blow　    始めますか？　y/n　>>> ')
     if options == 'y':
-        
+        digit = int(input("当てる値の桁数を決めてください。\n>>>"))
+        with open("record.json") as f:
+            jsn = json.load(f)
+        counter = jsn["{}".format(digit)]["count"]
+        count = counter
+        old_record = jsn["{}".format(digit)]["record"]
+        h_count=0
+        b_list=random.sample(range(1,9),int(digit))
+        print(b_list)
+        print("-------------------------------\n"\
+              "桁数 : {}\n"\
+              "制限回数 : {}\n"\
+              "現在の最速記録 : {}　　です。\n"\
+              "ゲームスタート！！\n"\
+              "( 途中で中止したい場合は'break'と入力してください )\n"\
+              "-------------------------------".format(digit, count, old_record))
         while True:
-            print('o:設定を開く')
-            print('s:ゲームをスタートする')
-            print('f:ゲームを終了する')
-            start=input('>>>')
-            if start == 's':
+            ass=input('入力して下さい(残り'+str(counter)+'回)...')
+            if ass=='break':
+                print('終了します')
+                break
+            elif ass=='hint':
+                if h_count==0:
+                    h=random.randint(1,4)
+                    print('ヒント:'+str(h)+'桁目の数字は'+str(b_list[int(h)-1])+'です')
+                    h_count+=1
+                else:
+                    print('ヒントは一回すでに使用しているので利用できません')
+            elif not ass.isdigit():
+                print('数字以外が入力されています')
+            elif len(ass)!=int(digit):
+                print('入力されたのが'+str(digit)+'桁ではありません')
+            else:
+                ass_list=list(map(int,ass))
+                print(ass_list)
 
-                f=open('Hit&Blow_digit.txt','r')
-                for row in f:
-                    digit=(row.strip())
-                f.close
-                h_count=0
-
-                print('スタート！！\n')
-                print('*/*/*/*/*/数字当てゲーム/*/*/*/*/*\n')
-                print('--<ルール説明>--------------------------------------------')
-                print('|  入力した数字のうち、値も順番もあっている数の個数をH   |')
-                print('|  値はあっているが順番が間違えている数の個数をBとする　 |')
-                print('|  ただし０を含まない'+str(digit)+'桁の数列で行う                     |')
-                print('|  なお中断したい場合は"break"を入力してください         |')
-                print('----------------------------------------------------------')
-                
-                b_list=random.sample(range(1,9),int(digit))
-                #print(b_list)
-
-                count=10
-
-                while True:
-
-                    ass=input('入力して下さい(残り'+str(count)+'回)...')
-                    if ass=='break':
-                        print('終了します')
-                        break
-                    elif ass=='hint':
-                        if h_count==0:
-                            h=random.randint(1,4)
-                            print('ヒント:'+str(h)+'桁目の数字は'+str(b_list[int(h)-1])+'です')
-                            h_count+=1
-                        else:
-                            print('ヒントは一回すでに使用しているので利用できません')
-                    elif not ass.isdigit():
-                        print('数字以外が入力されています')
-                    elif len(ass)!=int(digit):
-                        print('入力されたのが'+str(digit)+'桁ではありません')
-                    else:
-                        ass_list=list(map(int,ass))
-                        print(ass_list)
-
-                        H=0
-                        B=0
-                        for i,j in zip(ass_list,b_list):
-                            if i==j:
-                                H+=1
-                                B+=1
-                            elif i in b_list:
-                                B+=1
-                            else:
-                                pass
-                        count-=1
-                        if H==int(digit):
-                            print('クリアです')
-                            now_records=10-count
-                            f=open('Hit&Blow_records.txt','r')
-                            for row in f:
-                                old_records=(row.strip())
-                            f.close()
-
-                                
-                            if now_records < (int(old_records)):
-                                f=open('Hit&Blow_records.txt','w')
-                                f.write(str(now_records))
-                                f.close()
-                                print('***祝***')
-                                print('NEW RECORD:'+str(now_records))
-                                print('新記録として保存されます')
-
-                            else:
-                                print('現在の最高記録:'+str(old_records))
-                                print('次こそは更新しよう！')
-
-                            break
-
-                        if count==0:
-                            print('ゲームオーバー')
-                            print('正解:'+str(b_list))
-                            break
-
-                        print('ヒントです　　H:'+str(H)+'   B:'+str(B))
-                        print('------------------------------------------')
-            elif start == 'o':
-                print('<Options>')
-                while True:
-                    f=open('Hit&Blow_digit.txt','r')
-                    for row in f:
-                        digit=(row.strip())
-                    f.close
-                    f=open('Hit&Blow_records.txt','r')
-                    for row in f:
-                        old_records=(row.strip())
-                    f.close
-                    #print('r:これまでの記録を変更します['+str(old_records)+']')
-                    print('d:桁数を変更します['+str(digit)+']')
-                    print('f:<Options>を終了します')
-                    m=input('>>>')
-                    if m == 'd':
-                        while True:
-                            d=input('何桁に変更しますか？>>>')
-                            if not d.isdigit():
-                                print('数字以外が入力されています')
-                            elif int(d) >= 9 :
-                                print('正しく入力されていません')
-                            else:
-                                f=open('Hit&Blow_digit.txt','w')
-                                f.write(str(d))
-                                f.close
-                                break
-                    elif m == 'f':
-                        print('終了します')
-                        break
+                H=0
+                B=0
+                for i,j in zip(ass_list,b_list):
+                    if i==j:
+                        H+=1
+                        B+=1
+                    elif i in b_list:
+                        B+=1
                     else:
                         pass
-            elif start == 'f':
-                print('Hit&Blowを終了します')
-                break
-            else:
-                print('正しく入力されていません')
+                counter-=1
+                if H==int(digit):
+                    print('クリアです')
+                    new_record=count-counter                        
+                    if new_record < (int(old_record)):
+                        jsn["{}".format(digit)]["record"] = new_record
+                        with open("record.json", "w") as f:
+                            json.dump(jsn, f, indent=4)
+                        print('***祝***')
+                        print('NEW RECORD:'+str(new_record))
+                        print('新記録として保存されました！！')
 
+                    else:
+                        print('記録更新ならず...次こそは更新しよう！')
+
+                    break
+
+                if counter==0:
+                    print('ゲームオーバー')
+                    print('正解 : {}\n\n'.format(b_list))
+                    break
+
+                print('Hit:'+str(H)+'   Blow:'+str(B))
+                print('------------------------------------------')
     elif options == 'n':
         break
     else:
